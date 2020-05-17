bc190be5312184e66c75238eab487b888aeade18
Deborah Santillan <s.debb@yahoo.com>
2020-05-17 19:50:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,7 +6,7 @@ def my_collect(collection)
   i = 0 
   newCollection = []
   
-    while (i < collection.split(" ").first) do 
+    while (i < collection.length do 
       yield collection[i] 
  
   
