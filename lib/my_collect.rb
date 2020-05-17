b600bb6673014ce0d796e53deff697ec1ea86c4d
Deborah Santillan <s.debb@yahoo.com>
2020-05-17 20:37:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,23 @@
- 
+require "pry"  
 # collection = ["ruby", "javascript", "python", "objective-c"]
+
+# def my_collect(collection)
+ 
+#   i = 0 
+#   newCollection = []
+  
+#     while (i < collection.length) do 
+#       newCollection.push(yield my_collect)
+    
+  
+#     i += 1 
+
+#   end
+#   return newCollection
+  
+# end
+
+
 def my_collect(collection)
  
   i = 0 
