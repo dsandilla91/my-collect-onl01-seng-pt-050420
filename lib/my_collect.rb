c982dc9be33c328c046b99c22b5a63580f1e79b0
Deborah Santillan <s.debb@yahoo.com>
2020-05-17 20:36:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,13 @@
  
 # collection = ["ruby", "javascript", "python", "objective-c"]
-
 def my_collect(collection)
  
   i = 0 
   newCollection = []
   
     while (i < collection.length) do 
-      newCollection.push(yield my_collect)
-    
+      newCollection.push(yield my_collect) 
+ 
   
     i += 1 
 
@@ -16,20 +15,3 @@ def my_collect(collection)
   return newCollection
   
 end
-
-
-# def my_collect(collection)
- 
-#   i = 0 
-#   newCollection = []
-  
-#     while (i < collection.length) do 
-#       newCollection.push(yield my_collect) 
- 
-  
-#     i += 1 
-
-#   end
-#   return newCollection
-  
-# end
