54f18cb5feb0b2986038d83a7b31f95243aa0083
Deborah Santillan <s.debb@yahoo.com>
2020-05-17 20:34:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,20 +1,34 @@
 require "pry"  
 # collection = ["ruby", "javascript", "python", "objective-c"]
 
+# def my_collect(collection)
+ 
+#   i = 0 
+#   newCollection = []
+  
+#     while (i < collection.length) do 
+#       newCollection.push(yield my_collect)
+    
+  
+#   newCollection.push(collection[i].upcase)
+#     i += 1 
+
+#   end
+#   return newCollection
+  
+# end
 def my_collect(collection)
  
   i = 0 
   newCollection = []
   
     while (i < collection.length) do 
-      yield collection[i]
-    
+      newCollection.push(yield my_collect) 
+ 
   
-  newCollection.push(collection[i].upcase)
     i += 1 
 
   end
   return newCollection
   
 end
-
